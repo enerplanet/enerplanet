ed1b410cc359e8fa5eb890f5c8fc11dd32cd483b
skern <stefan.kern@th-deg.de>
2026-08-07 14:33:55 +0200

    update i18 implementation, add npm scripts for validation, added todo comment for parameters to update in later stage

---
+++ enerplanet/frontend/i18n-unused.config.cjs
@@ -0,0 +1,7 @@
+// i18n-unused.config.js
+// https://github.com/artemirq/i18n-unused#configuration
+/** @type {import('i18n-unused').RunOptions} */
+module.exports = {
+  localesPath: "src/locales",
+  srcPath: "src",
+};
